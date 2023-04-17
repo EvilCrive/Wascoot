f5b829f5d8dc7b4c32dd9866e2d150f2f214abc6
saraniknam <sara.niknamhesar@studenti.unipd.it>
2023-04-17 20:11:31 +0200

    feat: Some_changes_in_PaymentWithSubscriptionDatabase_and_Changing_Name

--- src/main/java/wascoot/database/PaymentWithSubscriptionDatabase.java
+++ src/main/java/wascoot/database/PaymentWithSubscriptionDatabase.java
@@ -9,13 +9,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class RevenueDatabase {
+public class PaymentWithSubscriptionDatabase {
 
     private static final String STATEMENT = "select production_to_month , sum(price) pr from ( SELECT DATE_TRUNC('month', withS.date_hour) AS production_to_month, SUM(s.fixed_price) AS price FROM paymentwithsubscription withS join usedsubscription u on withS.usedsubscription_id = u.id join subscription s on u.subscription_type = s.id GROUP BY DATE_TRUNC('month',withS.date_hour) union SELECT DATE_TRUNC('month', withoutS.date_hour) AS production_to_month, SUM(withoutS.price) AS price FROM paymentwithoutsubscription withoutS GROUP BY DATE_TRUNC('month',withoutS.date_hour) ) x group by production_to_month order by production_to_month;";
 
     private final Connection con;
 
-    public RevenueDatabase(Connection con) {
+    public PaymentWithSubscriptionDatabase(Connection con) {
         this.con = con;
     }
 
