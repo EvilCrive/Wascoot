46d3391f84603e9df6fa20024c7ec5eb7bc3ab33
EvilCrive <crivealby@gmail.com>
2023-04-21 20:51:08 +0200

    REST scooterrack get

--- src/main/java/wascoot/resource/Actions.java
+++ src/main/java/wascoot/resource/Actions.java
@@ -2,4 +2,5 @@ package wascoot.resource;
 
 public class Actions {
     public static final String INSERT_NEW_MODEL = "INSERT_NEW_MODEL";
+    public static final String GET_ALL_SCOOTERRACKS = "GET_ALL_SCOOTERRACKS";
 }
