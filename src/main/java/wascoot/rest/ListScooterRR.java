5565dc92f2d85127e32c196a0570816cd8533b93
shiva <shiva.gharehzad@studenti.unipd.it>
2023-04-25 16:04:18 -0700

    ListScooterRR.java

---
+++ src/main/java/wascoot/rest/ListScooterRR.java
@@ -0,0 +1,68 @@
+package wascoot.rest;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import wascoot.database.ScooterDatabase;
+import wascoot.resource.Actions;
+import wascoot.resource.Message;
+import wascoot.resource.ResourceList;
+import wascoot.resource.Scooter;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ListScooterRR {
+    package wascoot.rest;
+
+    import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import wascoot.database.ScooterRackDatabase;
+import wascoot.resource.Actions;
+import wascoot.resource.Message;
+import wascoot.resource.ResourceList;
+import wascoot.resource.Scooter;
+
+        import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+public final class ListScooterRR extends AbstractRR {
+   public ListScooterRR(HttpServletRequest req, HttpServletResponse res, Connection con) {
+                   super(Actions.GET_ALL_SCOOTER,req, res, con);
+               }
+
+           @Override
+  protected void doServe() throws IOException {
+
+                    List < Scooter > el = null;
+            Message m = null;
+
+                          try {
+
+                                   // creates a new DAO for accessing the database and lists the employee(s)
+                                           el = new ScooterDatabase(con).access().getOutputParam();
+
+                                  if (el != null) {
+                                  LOGGER.info("Employee(s) successfully listed.");
+
+                                           res.setStatus(HttpServletResponse.SC_OK);
+                                           new ResourceList<>(el).toJSON(res.getOutputStream());
+                                  } else { // it should not happen
+                                   LOGGER.error("Fatal error while listing employee(s).");
+
+                                            m = new Message("Cannot list employee(s): unexpected error.", "E5A1", null);
+                                    res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                                    m.toJSON(res.getOutputStream());
+                                }
+                       } catch (SQLException ex) {
+                           LOGGER.error("Cannot list employee(s): unexpected database error.", ex);
+
+                                    m = new Message("Cannot list employee(s): unexpected database error.", "E5A1", ex.getMessage());
+                           res.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                           m.toJSON(res.getOutputStream());
+                        }
+                }
+}
+}
