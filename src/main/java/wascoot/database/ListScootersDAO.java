6f0ffb9b5c7f4094cb0b637cc0d5c38c09527407
Alberto Crivellari <crivealby@gmail.com>
2023-06-03 23:32:53 +0200

    BUGFIX for error updated rest method, not servlet one

--- src/main/java/wascoot/database/ListScootersDAO.java
+++ src/main/java/wascoot/database/ListScootersDAO.java
@@ -99,6 +99,9 @@ public final class ListScootersDAO extends AbstractDAO<List<Scooter>> {
                 scooters.add(new Scooter(rs.getInt("id"), rs.getDate("date_of_purchase"), rs.getDouble("km_traveled"),
                         rs.getString("model"), rs.getDouble("remaining_battery_life"), rs.getInt("id_scooter_rack")));
             }
+
+            Collections.sort(scooters);
+
         } finally {
             if (rs != null) {
                 rs.close();
